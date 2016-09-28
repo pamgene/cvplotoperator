872db5db9bf5aef946d510da8fab1ad7fe5dd7c1
Rik de Wijn <rdwijn@nl.pamgene.com>
2016-09-28 17:28:10 +0200

    no deploy

--- R/cvstats.R
+++ R/cvstats.R
@@ -7,16 +7,73 @@
 #'aResult, a data frame with various per cell stats as columns.
 #' @export
 computeCVData = function(annotatedData) {
-
-  data = annotatedData$data %>% select(rowSeq,
-                                       colSeq,
-                                       y=value,
-                                       bOut=IsOutlier)
-  data = subset(data, !bOut)
-  aResult = data %>%group_by(rowSeq, colSeq) %>% summarise(m = mean(y), stdev = sd(y), cv = stdev/m, nreps = length(y))
+  colors = annotatedData$metadata$groupingType %in% "Color"
+  data = subset(annotatedData$data, !IsOutlier)
+  if(any(colors)){
+    Color = droplevels(interaction(data[, colors]))
+  } else {
+    Color = "Main"
+  }
+  data = data.frame(data, Color = Color)
+  aResult = data %>%group_by(rowSeq, colSeq) %>%
+    summarise( m = mean(value),
+               stdev = sd(value),
+               cv = stdev/m,
+               nreps = length(value),
+               lvar = var(log(value)),
+               pane = identity1(Color))
   return(aResult)
 }
 
+cvmodel = function(aResult, pLow = 0.05, pHigh = 0.98, maxIter = 25){
+  bLow 	= aResult$m <= quantile(aResult$m, pLow);
+  bHigh 	= aResult$m >= quantile(aResult$m, pHigh);
+  var = aResult$stdev^2
+
+  bModel = bLow|bHigh
+
+  ssq0 = NaN
+  ssq1 = NaN
+  pres = NaN
+  iter = 0
+  while(any(bModel)){
+    ssq0 = pooledVarEst( var[bLow], aResult$nreps[bLow]);
+    lssq1 = median(aResult$lvar[bHigh], na.rm = TRUE)
+    ssq1 = exp(lssq1) - 1
+    #calculate presence values for all spots
+    m0 = aResult$m;
+    m0[m0<0] = 0
+    pres = ( sqrt(ssq1) * m0) /(sqrt(ssq1)*m0 + sqrt(ssq0))
+
+    bLow = pres < pLow
+    bHigh = pres > pHigh
+
+    if (all(!bLow) | all(!bHigh)){
+      ssq0 = NaN
+      ssq1 = NaN
+      break
+    }
+    if ( all( (bLow|bHigh) == bModel) ){
+      break
+    } else {
+      bModel = bLow|bHigh
+      iter = iter + 1
+    }
+    if (iter > maxIter){
+      break
+    }
+  }
+  aResult = data.frame(aResult, ssq0 = ssq0, ssq1 = ssq1, pres = pres)
+  return(aResult)
+}
+
+
+identity1 = function(.).[1]
+
+pooledVarEst<-function(s2, n){
+  est = sum(s2 * (n-1), na.rm = TRUE ) / (sum(n) - length(n))
+  return(est)
+}
 
 
 
--- R/main.R
+++ R/main.R
@@ -1,6 +1,9 @@
 #' @import dplyr
 #' @import shiny
 #' @import bnutil
+#' @import ggplot2
+
+#
 #' @export
 operatorProperties = function() {
   aPropList = list(
@@ -16,31 +19,68 @@ shinyServerRun = function(input, output, session, context) {
     mainPanel(
       h4("Creating CV plots"),
       p("Please wait ..."),
-      verbatimTextOutput("done")
+      verbatimTextOutput("banner")
     )
   })
 
   getDataReactive = context$getData()
+  getFolderReactive = context$getFolder()
+  getPropertiesReactive = context$getPropertiesAsMap()
 
   observe({
 
     getData=getDataReactive$value
+    getFolder = getFolderReactive$value
+    getProperties = getPropertiesReactive$value
+
     if (is.null(getData)){
-      print('getData is null')
       return()
     }
 
     data = getData()
-
-    output$done = renderText({
-      result = computeCVData(data, progress=progress)
+    output$banner = renderText({
+      aResult = computeCVData(data)
+      forBn = !(colnames(aResult) %in% c("pane", "lvar"))
+      meta = data.frame(labelDescription = colnames(aResult)[forBn],
+                        groupingType = c("rowSeq", "colSeq", "QuantitationType","QuantitationType","QuantitationType", "QuantitationType"))
+      result = AnnotatedData$new(data = aResult[,forBn ], metadata = meta)
       context$setResult(result)
-      renderPrint({ 'done' })()
+      folder = getFolder()
+      save(file = file.path(folder, "runData.RData"), aResult)
+      return(folder)
     })
+    #stopApp()
   })
 }
 
+#' @export
+shinyServerShowResults = function(input, output, session, context) {
 
+  output$body = renderUI({
+    fluidPage(
+      titlePanel("CV plots"),
+      sidebarLayout(
+        sidebarPanel(h4("Controls")),
+        mainPanel(plotOutput("cvplot"))
+      )
+    )
+  })
+  getFolderReactive = context$getFolder()
 
+  observe({
 
+    getFolder = getFolderReactive$value
+    if (is.null(getFolder)) return()
+    folder = getFolder()
 
+    load(file.path(folder, "runData.RData"))
+
+    output$cvplot = renderPlot({
+      aResult = aResult %>% group_by(pane) %>% do(cvmodel(.))
+      aPlot = ggplot(aResult, aes(x = m, y = cv) ) + geom_point() + facet_wrap(~pane)
+      aPlot = aPlot + ylim(c(0, 0.5))
+      #print(aPlot)
+      return(aPlot)
+    })
+  })
+}
